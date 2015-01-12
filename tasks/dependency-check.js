1de33eca244df829d052b6855c71d9ffedd7bfd4
Pelle Wessman <pelle@kodfabrik.se>
2015-01-12 11:29:32 +0100

    added a grunt task

---
+++ tasks/dependency-check.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+  var check = require('../')
+
+  grunt.registerMultiTask('dependency-check', 'Matches used modules to listed dependencies', function () {
+    var done = this.async()
+
+    var options = this.options({
+      missing: true,
+      unused: true,
+      package: '.'
+    })
+
+    check({path: options.package, entries: this.filesSrc}, function(err, data) {
+      if (err) {
+        return grunt.fail.fatal(err)
+      }
+
+      var pkg = data.package
+      var deps = data.used
+      var results
+
+      if (options.unused) {
+        results = check.extra(pkg, deps)
+        if (results.length !== 0) {
+          grunt.log.error('Modules in package.json not used in code: ' + grunt.log.wordlist(results, {color: 'red'}))
+        }
+      }
+
+      if (options.missing) {
+        results = check.missing(pkg, deps)
+        if (results.length !== 0) {
+          grunt.fail.fatal('Dependencies not listed in package.json: ' + grunt.log.wordlist(results, {color: 'red'}))
+        }
+      }
+
+      done()
+    })
+  })
+}
