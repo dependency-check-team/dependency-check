30e703afe7e0c0bb0b8555af93cb340da16bde1d
Max Ogden <max@maxogden.com>
2015-02-12 09:56:48 -0800

    clean up help output

--- cli.js
+++ cli.js
@@ -10,13 +10,14 @@ if (args.version) {
 }
 
 if (args.help || args._.length === 0) {
-  console.log('\nUsage: dependency-check <path to package.json or module folder> [options]')
+  console.log('\nUsage: dependency-check <path to package.json or module folder> <options>')
 
   console.log('\nOptions:')
-  console.log('\n  --missing (default) -> Check to make sure that all modules in your code are listed in your package.json')
-  console.log('\n  --unused -> The inverse of the --missing check and will tell you which modules in your package.json *were not* used in your code')
-  console.log('\n  --no-dev -> Won\'t tell you about which devDependencies in your package.json dependencies that were not used in your code. Only usable with --unused')
-  console.log('\n  --entry -> By default your main and bin entries from package.json will be parsed, but you can add more the list of entries by passing them in as --entry')
+  console.log('--missing (default)   Check to make sure that all modules in your code are listed in your package.json')
+  console.log('--unused              The inverse of the --missing check and will tell you which modules in your package.json *were not* used in your code')
+  console.log('--no-dev              Won\'t tell you about which devDependencies in your package.json dependencies that were not used in your code. Only usable with --unused')
+  console.log('--entry               By default your main and bin entries from package.json will be parsed, but you can add more the list of entries by passing them in as --entry')
+  console.log("")
 
   process.exit(1)
 }
