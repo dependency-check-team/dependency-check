e88ae34537ee9dadbf472ffe7b32285f90fd99eb
Pelle Wessman <pelle@kodfabrik.se>
2022-02-22 17:52:05 +0100

    BREAKING: Drop Node 12
    Will fail silently on non-supported versions.
    
    Reason: The ecosystem is moving to >=14 as Node 12 is soon out of LTS so more and more modules has that as its minimum requirement, making it hard to claim to support more.

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 
 'use strict'
 
-if (require('./package.json').engines.node !== '^12.20.0 || ^14.13.1 || >=16.0.0') {
+if (require('./package.json').engines.node !== '^14.17.0 || >=16.0.0') {
   console.error('dependency-check: Mismatch between package.json node engine and cli engine check')
   process.exit(1)
 }
@@ -12,9 +12,9 @@ var match = process.version.match(/v(\d+)\.(\d+)/) || []
 var major = parseInt(match[1] || '', 10)
 var minor = parseInt(match[2] || '', 10)
 
-if (major >= 12 || (major === 12 && minor >= 20)) {
+if (major > 14 || (major === 14 && minor >= 17)) {
   require('./lib/cli-engine')
 } else {
-  console.error('dependency-check: Node 12.20.0 or greater is required. `dependency-check` did not run.')
+  console.error('dependency-check: Node 14.17.0 or greater is required. `dependency-check` did not run.')
   process.exit(0)
 }
